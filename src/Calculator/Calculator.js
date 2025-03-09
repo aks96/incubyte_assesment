2d10fe764442526c242ccd823d79fc66b32064e7
ankush kumar <kumar.ankush0796@gmail.com>
2025-03-09 19:38:24 +0000

    initial test single number

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,4 +1,4 @@
 export function add(numbers) {
   if (numbers === "") return 0;
-  return 0;
+  return parseInt(numbers);
 }
--- src/Calculator/Calculator.test.js
+++ src/Calculator/Calculator.test.js
@@ -3,3 +3,7 @@ import { add } from "./Calculator";
 test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("returns the number itself when a single number is provided", () => {
+  expect(add("1")).toBe(1);
+});
