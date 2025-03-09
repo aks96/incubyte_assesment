533f83b32611b5a8914092456e0e47c4516302e6
ankush kumar <kumar.ankush0796@gmail.com>
2025-03-09 19:40:04 +0000

    handle multiple numbers

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,4 +1,4 @@
 export function add(numbers) {
   if (numbers === "") return 0;
-  return parseInt(numbers);
+  return numbers.split(",").reduce((sum, num) => sum + parseInt(num), 0);
 }
--- src/Calculator/Calculator.test.js
+++ src/Calculator/Calculator.test.js
@@ -7,3 +7,6 @@ test("returns 0 for an empty string", () => {
 test("returns the number itself when a single number is provided", () => {
   expect(add("1")).toBe(1);
 });
+test("returns sum of two numbers", () => {
+  expect(add("1,5")).toBe(6);
+});
